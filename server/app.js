e432e6503dd9d080a51778c8d555206c2dd47c3d
betocostadev <robertomarx@gmail.com>
2020-04-22 09:39:59 -0300

    fixed server connection with the db

--- server/app.js
+++ server/app.js
@@ -21,7 +21,7 @@ app.use(function (req, res, next) {
 })
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost:27017/globomantics')
+mongoose.connect('mongodb://localhost:27017/globomantics', { useNewUrlParser: true, useUnifiedTopology: true })
 const db = mongoose.connection
 
 db.on('error', console.error.bind(console, 'connection error:'))
