eac07afb5b34b3e07829d2db5231e31a1c7b3011
betocostadev <robertomarx@gmail.com>
2020-04-23 10:00:59 -0300

    adding the store

---
+++ client/src/store/index.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+import user from './modules/user'
+import transactions from './modules/transactions'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  modules: {
+    user,
+    transactions
+  }
+})
---
+++ client/src/store/modules/user.js
@@ -0,0 +1,44 @@
+const state = {
+  email: '',
+  userId: null,
+  isLoggedIn: false,
+  loginError: ''
+}
+
+const getters = {
+  isLoggedIn: state => state.isLoggedIn,
+  userId: state => state.userId,
+  loginError: state => state.loginError
+}
+
+const actions = {
+  actions: {
+    logInUser ({commit}, payload) {
+      if (payload.email === 'test1@user.com' && payload.password === 'test111') {
+        payload.userId = '5a777f0a75f64a1698221d98'
+        commit('logInUser', payload)
+      } else {
+        commit('loginError')
+      }
+    }
+  }
+}
+
+const mutations = {
+  logInUser (state, payload) {
+    state.email = payload.email
+    state.userId = payload.userId
+    state.isLoggedIn = true
+  },
+  LoginError (state) {
+    state.isLoggedIn = false
+    state.loginError = 'Email and/or password are invalid. Login failed, please check your information.'
+  }
+}
+
+export default {
+  state,
+  getters,
+  actions,
+  mutations
+}
